61771db33c14e9e294ea897216360d63fbf91739
MrClemds <clement.pont@mrclemds.fr>
2019-02-07 15:29:04 +0100

    feat(Service): Add some query params to be more flexible

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,17 @@ const puppeteer = require('puppeteer');
 
 export class Server {
   public app: Express.Express;
+  public pdfParams: any;
 
   constructor() {
     this.app = Express.default();
+
+    this.pdfParams = {};
+
     this.app.use((req: Express.Request, res: Express.Response) => {
       const url = req.query.url;
       this.checkRequest(url, req.query.api, res).then(() => {
+        this.readPDFParams(req.query);
         this.generatePDF(url, res);
       }).catch((error) => {
         return error;
@@ -20,27 +25,57 @@ export class Server {
   public startServer() {
     return this.app.listen(process.env.PORT || 8080, (err: any) => {
       if (err) return console.error(err);
-      else if (process.env.NODE_ENV === 'development') return console.log('server ready');
-
+      else if (process.env.NODE_ENV === 'development') return console.log("\x1b[35m\x1b[47m", "server ready", "\x1b[0m");
     });
   }
 
   private checkRequest(url: string, apiKey: string, res: Express.Response) {
     return new Promise((resolve, reject) => {
-      console.log(process.env.API_KEY);
-      if (!url || !apiKey) {
+      if (!url || (!!process.env.API_KEY && !apiKey)) {
         reject(res.status(400).send({
-          success: false,
           message: "Bad call",
         }));
       } else if (!!process.env.API_KEY && apiKey != process.env.API_KEY) {
         reject(res.status(401).send({
-          success: false,
           message: "Bad API key",
         }));
       }
       resolve();
     })
+  }
+
+  private readPDFParams(queyParams: any) {
+    Object.keys(queyParams).forEach(param => {
+      if (!this.pdfParams.margin) this.pdfParams.margin = {};
+      switch(param) {
+        case 'format':
+          this.pdfParams.format = queyParams.format;
+          break;
+        case 'scale':
+          this.pdfParams.scale = +queyParams.scale;
+          break;
+        case 'marginTop':
+          this.pdfParams.margin.top = queyParams.marginTop;
+          break;
+        case 'marginLeft':
+          this.pdfParams.margin.left = queyParams.marginLeft;
+          break;
+        case 'marginBottom':
+          this.pdfParams.margin.bottom = queyParams.marginBottom;
+          break;
+        case 'marginRight':
+          this.pdfParams.margin.right = queyParams.marginRight;
+          break;
+        case 'width':
+          this.pdfParams.width = queyParams.width;
+          break;
+        case 'height':
+          this.pdfParams.height = queyParams.height;
+          break;
+        case 'tilte':
+          this.pdfParams.title = queyParams.title;
+      }
+    });
   }
 
   private async generatePDF(url: string, res: Express.Response) {
@@ -51,21 +86,20 @@ export class Server {
 
       const page = await browser.newPage();
       await page.goto(url);
-  
-      const pdf = await page.pdf({ 
-          format: 'A4',
-          scale: 2,
-          margin: { top: '15px', left: '10px', bottom: '10px', right: '10px' }
-        });
+
+      if (!this.pdfParams.title) this.pdfParams.title = await page.title();
+      
+      const pdf = await page.pdf(this.pdfParams);
 
       browser.close();
-    
-      res.set('Content-Type', 'application/pdf');
+
+      res.setHeader('Content-Description', 'File Transfer');
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=' + this.pdfParams.title + '.pdf');
       res.send(pdf);
     } catch(err) {
       res.status(500).send({
-        success: false,
-        message: "Puppeteer error",
+        message: err.message,
       });
       console.log(err);
     }
